29131b35e17982934420a30c3ad2231fec5b6ee3
aaryandidwania <aaryandidwania5@gmail.com>
2026-01-16 23:34:39 +0530

    commit 1 : Friday , 16th January 2026 , 11:32 PM

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Welcome $name!",
+        text = "Hello $name!",
         modifier = modifier
     )
 }
